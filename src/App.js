d28938a6f5c1d6cb35275d250382247f0a53e8de
Tyler Krings <w.tyler.krings@gmail.com>
2020-10-26 22:45:51 -0500

    local works

--- src/App.js
+++ src/App.js
@@ -1,62 +1,88 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listTodos } from './graphql/queries';
-import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { API, Storage } from 'aws-amplify';
 
 const initialFormState = { name: '', description: '' }
 
 function App() {
-  const [notes, setTodos] = useState([]);
+  const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
   useEffect(() => {
-    fetchTodos();
+    fetchNotes();
   }, []);
 
-  async function fetchTodos() {
-    const apiData = await API.graphql({ query: listTodos });
-    setTodos(apiData.data.listTodos.items);
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+      return note;
+    }))
+    setNotes(apiData.data.listNotes.items);
   }
 
-  async function createTodo() {
+  async function createNote() {
     if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createTodoMutation, variables: { input: formData } });
-    setTodos([ ...notes, formData ]);
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setNotes([ ...notes, formData ]);
     setFormData(initialFormState);
   }
 
-  async function deleteTodo({ id }) {
-    const newTodosArray = notes.filter(note => note.id !== id);
-    setTodos(newTodosArray);
-    await API.graphql({ query: deleteTodoMutation, variables: { input: { id } }});
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
+  async function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
   }
 
   return (
     <div className="App">
-      <h1>My Todos App</h1>
+      <h1>My Notes App</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Todo name"
+        placeholder="Note name"
         value={formData.name}
       />
       <input
         onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Todo description"
+        placeholder="Note description"
         value={formData.description}
       />
-      <button onClick={createTodo}>Create Todo</button>
+      <input
+  type="file"
+  onChange={onChange}
+/>
+      <button onClick={createNote}>Create Note</button>
       <div style={{marginBottom: 30}}>
         {
-          notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteTodo(note)}>Delete note</button>
-            </div>
-          ))
-        }
+      notes.map(note => (<div key={note.id || note.name}>
+      <h2>{note.name}</h2>
+      <p>{note.description}</p>
+      <button onClick={() => deleteNote(note)}>Delete note</button>
+      {
+        note.image && <img src={note.image} style={{width: 400}} />
+      }
+    </div>
+  ))
+}
       </div>
       <AmplifySignOut />
     </div>
